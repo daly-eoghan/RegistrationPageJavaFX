c43e152f055f60a958f8dc36bcc3ecfbb6865333
Eoghan <119323056@umail.ucc.ie>
2021-12-03 15:06:51 +0000

    Added First test class for email. This is the first failing test.

---
+++ src/test/java/EmailValidatorTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmailValidatorTest {
+    private EmailValidator emailValidator;
+
+    @BeforeEach
+    public void setUp() {
+        emailValidator = new EmailValidator();
+    }
+
+    @Test
+    public void validateEmail() {
+        //Blank for now
+    }
+}
