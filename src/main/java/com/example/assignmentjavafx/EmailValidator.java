59de28ee724f146c9e992f304376dcd5a0a70aca
Eoghan <119323056@umail.ucc.ie>
2021-12-04 20:43:12 +0000

    Rewrote the test function in EmailValidator.java to test for an appropriate email using regex.

--- src/main/java/com/example/assignmentjavafx/EmailValidator.java
+++ src/main/java/com/example/assignmentjavafx/EmailValidator.java
@@ -1,9 +1,12 @@
 package com.example.assignmentjavafx;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.function.Predicate;
 
 public class EmailValidator implements Predicate<String> {
     @Override
-    public boolean test(String s) {
-        return true;
+    public boolean test(@NotNull String email) {
+
+        return email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z0-9]{2,6}$");
     }
 }
