50b37486e0619dbe4bfb85c45a8cf44a7b2add52
Eoghan <119323056@umail.ucc.ie>
2021-12-04 21:04:55 +0000

    Added First test class for Password. This is the first failing test.

---
+++ src/test/java/PasswordValidatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PasswordValidatorTest {
+    private PasswordValidator passwordValidator;
+
+    @BeforeEach
+    public void setUp() {
+        passwordValidator = new PasswordValidator();
+    }
+
+    @Test
+    public void validatePassword() {
+        //Blank for now
+    }
+}
